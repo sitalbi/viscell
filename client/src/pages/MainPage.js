77bea843d213764f68b7d5c2fcc47c70ef99294c
Martin Ithurbide <martin.ithurbide@etu.u-bordeaux.fr>
2024-02-13 17:24:06 +0100

    (feat) set up the project architecture

---
+++ client/src/pages/MainPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Sankey} from "../components/Sankey";
+
+export const MainPage = () => {
+    return (
+        <div className="main-page-principal-div">
+        <h1>Viscell</h1>
+        <Sankey />
+        </div>
+    );
+};
