18c2fa0c197876973305a96ad4e812b37b9ea8f2
Valentin Leroy <valentin.leroy@etu.u-bordeaux.fr>
2024-02-15 16:26:10 +0100

    (feat) added working tooltip and formatted code

--- client/src/Components/Sankey.js
+++ client/src/Components/Sankey.js
@@ -1,10 +1,17 @@
+import ReactDOM from "react-dom/client";
 import React, { useRef, useEffect } from "react";
+
 import * as d3 from "d3";
 import { select as d3Select } from "d3-selection";
-import ReactDOM from "react-dom/client";
 import { sankey, sankeyLinkHorizontal } from "d3-sankey";
-import BarplotNode from "./Barplot";
 
+import Barplot from "./Barplot";
+
+/**
+ * Sankey component
+ * 
+ * @returns {JSX.Element}
+ */
 export function Sankey() {
   const svgRef = useRef();
 
@@ -23,14 +30,12 @@ export function Sankey() {
         { name: "Z", value: 22 }
       ],
       links: [
-        { source: 0, target: 2, value: 5, info: "Info about link 1" },
-        { source: 0, target: 3, value: 7, info: "Info about link 2" },
-        { source: 1, target: 2, value: 3, info: "Info about link 3" },
-        { source: 1, target: 3, value: 9, info: "Info about link 4" },
-        { source: 2, target: 4, value: 2, info: "Info about link 5" },
-        { source: 2, target: 5, value: 9, info: "Info about link 6" },
-        { source: 3, target: 6, value: 4, info: "Info about link 7" },
-        { source: 3, target: 7, value: 9, info: "Info about link 8" }
+        { source: 1, target: 2, value: 3, info: "Info about link 1" },
+        { source: 1, target: 3, value: 9, info: "Info about link 2" },
+        { source: 2, target: 4, value: 2, info: "Info about link 3" },
+        { source: 2, target: 5, value: 9, info: "Info about link 4" },
+        { source: 3, target: 6, value: 4, info: "Info about link 5" },
+        { source: 3, target: 7, value: 9, info: "Info about link 6" }
       ]
     };
 
@@ -55,39 +60,54 @@ export function Sankey() {
       .attr("stroke", "#000")
       .attr("stroke-opacity", 0.5)
       .attr("stroke-width", d => d.value * 10)
-      .on("mouseover", function(d) {
+      .on("mouseover", function (event, d) {
         d3.select(this)
           .attr("stroke-opacity", 1)
-          .append("title")
-          .text(d.info);
+          .attr("cursor", "pointer");
+
+        const [x, y] = d3.pointer(event);
+
+        // Show tooltip
+        const tooltip = d3.select(this.parentNode)
+          .append("foreignObject")
+          .attr("class", "tooltip")
+          .attr("x", x)
+          .attr("y", y)
+          .attr("width", 60)
+          .attr("height", 30)
+          .style("background-color", "white")
+          .style("border", "1px solid black")
+          .style("padding", "5px")
+          .style("border-radius", "5px");
+
+        tooltip.append("xhtml:div")
+          .html(`${d.source.name} -> ${d.target.name}`);
       })
-      .on("mouseout", function() {
-        d3.select(this)
-          .attr("stroke-opacity", 0.5)
-          .select("title")
-          .remove();
+      .on("mouseout", function () {
+        d3.select(this).attr("stroke-opacity", 0.5);
+
+        // Remove tooltip
+        d3.select(this.parentNode).selectAll(".tooltip").remove();
       });
 
-    // Draw nodes as BarplotNode components
+    // Draw nodes as Barplot components
     svg
       .append("g")
       .selectAll(".node")
       .data(nodes)
       .join("g")
       .attr("class", "node")
-      .each(function(d) {
+      .each(function (d) {
         const foreignObject = d3Select(this)
           .append("foreignObject")
           .attr("x", d.x0)
           .attr("y", d.y0)
           .attr("width", d.x1 - d.x0)
           .attr("height", d.y1 - d.y0);
-
         const div = foreignObject.append("xhtml:div");
-        const component = <BarplotNode x={d.x0} y={d.y0} width={d.x1 - d.x0} height={d.y1 - d.y0} />;
+        const component = <Barplot x={d.x0} y={d.y0} width={d.x1 - d.x0} height={d.y1 - d.y0} />;
         ReactDOM.createRoot(div.node()).render(component);
       });
-
   }, []);
 
   return (
