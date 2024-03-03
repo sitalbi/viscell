f09cd562bbebd62b8c6a9e0b2151f7427697d7dc
Valentin Leroy <valentin.leroy@etu.u-bordeaux.fr>
2024-03-03 15:59:03 +0100

    (fix) passed pipeline before fixing authors

--- client/src/components/Barplot.js
+++ client/src/components/Barplot.js
@@ -4,70 +4,66 @@ import * as d3 from "d3";
 const Barplot = ({ width, height, cellName, genes }) => {
   const svgRef = useRef();
 
-  var onBarClick = function() {
+  var onBarClick = function () {
     const geneName = d3.select(this).data()[0];
-    
     // go to hyperlink
     window.open(`https://pubchem.ncbi.nlm.nih.gov/gene/${geneName}/Homo_sapiens`);
-
   }
 
-
-  var mouseOverBar = function() {
+  var mouseOverBar = function () {
     d3.select(this).attr("opacity", 0.5);
   }
 
-  var mouseOutBar = function(){
+  var mouseOutBar = function () {
     d3.select(this).attr("opacity", null);
   }
 
-  // Draw barplot
-  function drawBarplot(g, data, labels, scaledWidth, scaledHeight, yScale) {
-    g.selectAll("rect")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("x", (d, i) => i * (scaledWidth / data.length))
-    .attr("y", d => yScale(d))
-    .attr("width", (scaledWidth / data.length - 4)) // Augmenter l'écart entre les colonnes
-    .attr("height", d => scaledHeight - yScale(d))
-    .attr("fill", "steelblue")
-    .attr("data-testid", (d, i) => `bar-${labels[i]}`); // Ajouter un data-testid basé sur le nom du gène
-
-
-    // Add legend text
-    g.selectAll("text")
-      .data(labels)
-      .enter()
-      .append("text")
-      .text(d => d)
-      .attr("x", (d, i) => i * (scaledWidth / labels.length) + (scaledWidth / labels.length - 10) / 2) // Ajuster la position en X
-      .attr("y", scaledHeight) // Ajuster la position en Y
-      .attr("text-anchor", "middle")
-      .attr("font-size", "10px")
-      .attr("fill", "black");
-
-    // Add on BarClick
-    g.selectAll("rect")
-      .on("click", onBarClick)
-      .data(labels)
-      .attr("name", d => d)
-      .on('mouseover', mouseOverBar)
-      .on('mouseout', mouseOutBar);
-
-
-    // Add title
-    g.append("text")
-      .text(`Title : ${cellName}`)
-      .attr("x", scaledWidth / 2)
-      .attr("y", scaledHeight /2)
-      .attr("text-anchor", "middle")
-      .attr("font-size", "12px")
-      .attr("font-weight", "bold")
-      .attr("fill", "black");
-  }
-
   useEffect(() => {
+    function drawBarplot(g, data, labels, scaledWidth, scaledHeight, yScale) {
+      g.selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", (d, i) => i * (scaledWidth / data.length))
+        .attr("y", d => yScale(d))
+        .attr("width", (scaledWidth / data.length - 4)) // Augmenter l'écart entre les colonnes
+        .attr("height", d => scaledHeight - yScale(d))
+        .attr("fill", "steelblue")
+        .attr("data-testid", (d, i) => `bar-${labels[i]}`); // Ajouter un data-testid basé sur le nom du gène
+
+      // Add legend text
+      g.selectAll("text")
+        .data(labels)
+        .enter()
+        .append("text")
+        .text(d => d)
+        .attr("x", (d, i) => i * (scaledWidth / labels.length) + (scaledWidth / labels.length - 10) / 2) // Ajuster la position en X
+        .attr("y", scaledHeight) // Ajuster la position en Y
+        .attr("text-anchor", "middle")
+        .attr("font-size", "10px")
+        .attr("fill", "black");
+
+      // Add on BarClick
+      g.selectAll("rect")
+        .on("click", onBarClick)
+        .data(labels)
+        .attr("name", d => d)
+        .on('mouseover', mouseOverBar)
+        .on('mouseout', mouseOutBar);
+
+
+      // Add title
+      g.append("text")
+        .text(`Title : ${cellName}`)
+        .attr("x", scaledWidth / 2)
+        .attr("y", scaledHeight / 2)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "12px")
+        .attr("font-weight", "bold")
+        .attr("fill", "black");
+    }
+
+
     const svg = d3.select(svgRef.current);
     svg.selectAll("*").remove();
 
@@ -101,7 +97,7 @@ const Barplot = ({ width, height, cellName, genes }) => {
       // Draw bars
       drawBarplot(g, data, labels, scaledWidth, scaledHeight, yScale);
     }
-    
+
   }, [width, height, cellName, genes]);
 
   // Declare style for the barplot
