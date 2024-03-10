6508e56189a24f216e42c4cdd25ef69bdd8bad6b
Adrien Le Corre <adrien.le-corre@u-bordeaux.fr>
2024-03-10 21:43:44 +0100

    (fix) Added Accordion

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,19 +1,29 @@
 import { React } from "react";
-import { Container, Row, Col } from 'react-bootstrap';
-
+import { Container, Row, Col, Accordion } from "react-bootstrap";
 
 export const About = () => {
   return (
     <Container className="d-flex justify-content-center">
-            <Row>
-                <Col>
-          <div  className="justify-content-center">
-                <h1 id="viscell">viscell</h1>
-                <p>
-                  <strong>Superviseur :</strong> Asloudj Yanis
-                  (yasloudj@u-bordeaux.fr)
-                </p>
-                <h2 id="contexte">Contexte</h2>
+      <Row>
+        <Col>
+          <div className="justify-content-center">
+            <h1 id="viscell">viscell</h1>
+            <p>
+              <strong>Superviseur :</strong> Asloudj Yanis
+              (yasloudj@u-bordeaux.fr)
+              <p>
+              <strong>Développeurs :</strong>
+              <ul>
+                <li>Nikolaï Amossé</li>
+                <li>Martin Ithurbide</li>
+                <li>Adrien Le Corre</li>
+                <li>Valentin Leroy</li>
+                <li>Yusuf Senel</li>
+                <li>Simon Talbi</li>
+              </ul>
+            </p>
+            </p>
+            <h2 id="contexte">Contexte</h2>
                 <p>
                   Dans le cadre de l'UE Projet de Fin d&#39;Études du Master 2 Informatique de l'Université de Bordeaux, cette application a été réalisée par la demande de Yanis Asloudj, doctorant en Bio-Informatique.
                   Cette dernière est un champ de recherche à
@@ -47,46 +57,35 @@ export const About = () => {
                   l&#39;étude des populations de cellules, i.e. leur taille,
                   leurs gènes et leur fiabilité.
                 </p>
-                <h2 id="objectifs">Objectifs</h2>
-                <p>
-                  Pour explorer les résultats d&#39;une analyse single-cell,
-                  Yanis Asloudj a conceptualisé une métaphore visuelle simple, axée
-                  autour de plusieurs barplots, représentant une population de
-                  cellules chacun. Le contenu d&#39;un barplot représente les
-                  gènes caractéristiques d&#39;une population, tandis que son
-                  positionnement sur l’écran représente les liens de parenté
-                  avec les autres populations.
-                </p>
-                <p>
-                  Cette métaphore visuelle a été implémentée par notre équipe de développement dans un outil client basé sur
-                  les nouvelles technologiques de développement du Web.
-                </p>
-                <h2 id="description-rapide-du-projet">
-                  Description rapide du projet
-                </h2>
-                <p>
-                  <code>TODO</code>
-                </p>
-                <h2 id="technologies">Technologies</h2>
-                <p>
-                  <code>TODO</code>                
-                </p>
-                <h2 id="equipe">Équipe</h2>
-                <p>
-                <strong>Développeurs :</strong>
-                <ul>
-                    <li>Nikolaï Amossé</li>
-                    <li>Martin Ithurbide</li>
-                    <li>Adrien Le Corre</li>
-                    <li>Valentin Leroy</li>
-                    <li>Yusuf Senel</li>
-                    <li>Simon Talbi</li>
-                </ul>
-                
-                </p>
+            <Accordion defaultActiveKey="0" alwaysOpen>
+              <Accordion.Item eventKey="1">
+                <Accordion.Header id="objectifs"><h2>Objectifs</h2></Accordion.Header>
+                <Accordion.Body>
+                  <p>
+                    Pour explorer les résultats d&#39;une analyse single-cell,
+                    Yanis Asloudj a conceptualisé une métaphore visuelle simple,
+                    axée autour de plusieurs barplots, représentant une
+                    population de cellules chacun. Le contenu d&#39;un barplot
+                    représente les gènes caractéristiques d&#39;une population,
+                    tandis que son positionnement sur l’écran représente les
+                    liens de parenté avec les autres populations.
+                  </p>
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="2">
+                <Accordion.Header id="fonctionnalites"><h2>Fonctionnalités</h2></Accordion.Header>
+                <Accordion.Body>
+                  <p>
+                    <code>TODO</code>
+                  </p>
+                </Accordion.Body>
+              </Accordion.Item>
+
+            </Accordion>
+            
           </div>
-          </Col>
-            </Row>
-        </Container>
+        </Col>
+      </Row>
+    </Container>
   );
 };
