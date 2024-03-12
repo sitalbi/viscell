e4d2ecd880bc7dac3d6cb9a4189a104a37f95a75
Yusuf Senel <yusuf.senel@etu.u-bordeaux.fr>
2024-03-12 19:07:45 +0100

    (feat/test) removed Open button, instead all the svg is clickable & corrected unit tests

--- client/src/tests/Barplot.test.js
+++ client/src/tests/Barplot.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor, toBeNull } from '@testing-library/react';
 import Barplot from '../components/Barplot';
 
 
@@ -40,26 +40,54 @@ describe('Barplot component', () => {
         expect(screen.queryByText('Gene6')).not.toBeInTheDocument(); // Ensure 4th maximum gene is not present
     });
 
-    it('renders Barplot with modal', () => {
-        render(<Barplot width={400} height={200} cellName="TestCell" genes={genesMap} colorMap={colorMap}/>);
+    it('renders full Barplot with modal', () => {
+        render(<Barplot width={400} height={200} cellName="TestCell" genes={genesMap} colorMap={colorMap} />);
 
-        // Click on the title to open the modal
-        fireEvent.click(screen.getByText(/Open/i));
+        // Click on the svg
+        fireEvent.click(screen.getByTestId('barplot-svg'));
 
         // Check if the modal is rendered
-        const modalElement = screen.getByText(/Population/i); // Room for improvement
+        const modalElement = screen.getByText(/Population/i);
         expect(modalElement).toBeInTheDocument();
+
+        // Check the number of genes
+        const numberOfGenes = screen.getByText(/Total number of genes for TestCell : 6/i);
+        expect(numberOfGenes).toBeInTheDocument();
 
     });
 
-    it('closes the modal when "Close" button is clicked', () => {
-        render(<Barplot width={400} height={200} cellName="TestCell" genes={new Map()} colorMap={colorMap}/>);
+    it('renders with all genes after clicking the title', async () => {
+        render(<Barplot width={500} height={300} cellName="C6" genes={genesMap} colorMap={colorMap} />);
 
-        // Click on the title to open the modal
-        fireEvent.click(screen.getByText(/Open/i));
+        // Click on the svg
+        fireEvent.click(screen.getByTestId('barplot-svg'));
+
+        // Wait for the modal to be rendered
+        await waitFor(() => {
+            expect(screen.getByText(/Population/i)).toBeInTheDocument();
+        });;
+
+        const totalGenes = screen.getByText(/Total number of genes for C6 : 6/i);
+        expect(totalGenes).toBeInTheDocument();
+
+        // Check if the genes are rendered
+        expect(await screen.findByText('Gene5')).toBeInTheDocument();
+        expect(await screen.findByText('Gene4')).toBeInTheDocument();
+        expect(await screen.findByText('Gene2')).toBeInTheDocument();
+        expect(await screen.findByText('Gene6')).toBeInTheDocument();
+        expect(await screen.findByText('Gene3')).toBeInTheDocument();
+        expect(await screen.findByText('Gene1')).toBeInTheDocument();
+    });
+
+
+    it('closes the modal when "Close" button is clicked', () => {
+        render(<Barplot width={400} height={200} cellName="TestCell" genes={new Map()} colorMap={colorMap} />);
+
+        // Click on the svg
+        fireEvent.click(screen.getByTestId('barplot-svg'));
 
         // Check if the modal is rendered
-        const modalElement = screen.getByText(/Population/i); // Room for improvement
+        const modalElement = screen.getByText(/Population/i);
         expect(modalElement).toBeInTheDocument();
 
         // Click on the "Close" button
@@ -68,6 +96,32 @@ describe('Barplot component', () => {
         // Check if the modal is closed
         expect(modalElement).not.toBeInTheDocument();
     });
+
+    it('renders the correct information on mouseover on a bar and reset style on mouseout', () => {
+        render(<Barplot width={400} height={200} cellName="TestCell" genes={genesMap} colorMap={colorMap} />);
+
+        // Get a reference to the rectangle element
+        const barElement = screen.getByTestId('bar-Gene5');
+        fireEvent.mouseOver(barElement);
+
+        // Assertions : check if opacity and cursor are updated
+        expect(barElement).toHaveAttribute('opacity', '0.5');
+        expect(barElement).toHaveStyle('cursor: pointer');
+
+        // Check if the tooltip is created and contains the correct gene name
+        expect(document.querySelector('.tooltip')).toBeInTheDocument();
+        expect(document.querySelector('.tooltip')).toHaveTextContent('Gene: Gene5');
+
+        // Mouse out
+        fireEvent.mouseOut(barElement);
+
+        // Assertions : check if opacity and cursor are reset
+        expect(barElement.getAttribute('opacity')).toBeNull();
+
+        // Check if the tooltip is removed
+        expect(document.querySelector('.tooltip')).not.toBeInTheDocument();
+    });
+
 
     it('clicking on a bar opens the correct hyperlink', () => {
         const width = 300;
@@ -78,7 +132,7 @@ describe('Barplot component', () => {
             ["ACTC1", 1],
             ["ACTG2", 1]
         ]);
-        const { getByTestId } = render(<Barplot width={width} height={height} cellName={cellName} genes={genes} colorMap={colorMap}/>);
+        const { getByTestId } = render(<Barplot width={width} height={height} cellName={cellName} genes={genes} colorMap={colorMap} />);
         const barElement = getByTestId('bar-ACTA2');
         const originalOpen = window.open;
         window.open = jest.fn();
