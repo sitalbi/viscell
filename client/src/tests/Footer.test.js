5e2b939a5e9fcc0c8569f0a84ea4c72749e69213
Adrien Le Corre <adrien.le-corre@u-bordeaux.fr>
2024-03-14 23:28:33 +0100

    (test) Footer tested

---
+++ client/src/tests/Footer.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { Footer } from "../components/Footer";
+
+describe("Footer component", () => {
+  test("renders footer with correct content", () => {
+    const { getByText } = render(<Footer />);
+    expect(
+      getByText(
+        "Authors : Nikolaï Amossé, Martin Ithurbide, Adrien Le Corre, Valentin Leroy, Yusuf Senel, Simon Talbi"
+      )
+    ).toBeInTheDocument();
+    expect(getByText("Property of the University of Bordeaux ©2024")).toBeInTheDocument();
+  });
+});
