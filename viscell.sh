38eda6252a049f759955c81dcb3a23cd4c2812c8
Martin Ithurbide <martin.ithurbide@etu.u-bordeaux.fr>
2024-03-20 13:35:41 +0100

    (feat) rename executable and stronger script

---
+++ viscell.sh
@@ -0,0 +1,23 @@
+#######################################
+# Executable of the Viscell application
+#######################################
+
+# navigate to the client directory
+cd client;
+
+# check if node is installed
+if ! [ -x "$(command -v npm)" ]; then
+
+    if [ -x "$(command -v apt-get)" ]; then
+        sudo apt-get install npm;
+    else
+        echo 'Error: npm is not installed.' >&2
+        exit 1
+    fi
+fi
+
+# install the dependencies
+npm install;
+
+# start the client
+npm start;
